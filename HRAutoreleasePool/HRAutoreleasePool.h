d4d1225ed51fa5df318b15037506d8c329273c37
Dmitry Rykun <drykun@readdle.com>
2020-02-07 15:45:16 +0200

    Initial Commit

---
+++ HRAutoreleasePool/HRAutoreleasePool.h
@@ -0,0 +1,19 @@
+//
+//  HRAutoreleasePool.h
+//  HRAutoreleasePool
+//
+//  Created by Dmitry Rykun on 2/7/20.
+//  Copyright © 2020 Dmitry Rykun. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HRAutoreleasePool.
+FOUNDATION_EXPORT double HRAutoreleasePoolVersionNumber;
+
+//! Project version string for HRAutoreleasePool.
+FOUNDATION_EXPORT const unsigned char HRAutoreleasePoolVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HRAutoreleasePool/PublicHeader.h>
+
+
